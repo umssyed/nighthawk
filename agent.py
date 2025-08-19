738a14dde71635a3494f805d48e32d6cbd18b197
Uzair Syed <umssyed@gmail.com>
2025-08-19 17:10:24 -0600

    Network Monitoring: Port Scans Implementation

--- agent.py
+++ agent.py
@@ -5,20 +5,23 @@ import logging
 
 # Import monitoring modules
 from modules.process_monitor import main as run_process_monitor
+from modules.network_monitor import main as run_network_monitor
 
 def run_modules():
     print(f"🦉 NightWatcher v1.0 is running... Press Ctrl+C to stop.\n")
     try:
         # Run each module synchronously as threads
-        proc_mon_thread = threading.Thread(target=run_process_monitor)
+        proc_monitor_thread = threading.Thread(target=run_process_monitor)
+        net_monitor_thread = threading.Thread(target=run_network_monitor)
 
 
         # Start each thread
-        proc_mon_thread.start()
-
+        proc_monitor_thread.start()
+        net_monitor_thread.start()
 
         # Join threads to complete before next cycle
-        proc_mon_thread.join()
+        proc_monitor_thread.join()
+        net_monitor_thread.join()
 
     except Exception as e:
         logging.error(f"Exception while running modules: {e}")
--- modules/network_monitor.py
+++ modules/network_monitor.py
@@ -2,18 +2,33 @@ from scapy.all import sniff, IP_PROTOS, TCP_SERVICES, UDP_SERVICES
 from datetime import datetime
 import socket
 import requests
-import psutil
+import psutil, os, time
 import logging
+from config import CPU_THRESHOLD, MEMORY_THRESHOLD, SCAN_INTERVAL_SECONDS
+
+# Port Scanning for malicious activity
+# Port 20 (UDP) - File Transfer Protocol for data transfer
+# Port 22 (TCP) - SSH protocol for secure logins, FTP and port forwarding
+# Port 23 (TCP) - Telenet protocol for unencrypted text commutations
+# Port 53 (UDP) - DNS translates names of all computer on internet to IP address
+# Port 80 (TCP) - Internet HTTP
 
 
 def process_dns_packet(pkt):
+    pass
 
 
-
-def start_dns_monitor():
+def start_monitor():
     print(f"[*] Starting DNS monitoring...")
     sniff(
         filter="udp port 53",
         prn= process_dns_packet,
         store=0
     )
+
+
+def main():
+    print(f"== Network Monitor Thread ==")
+    while True:
+        start_monitor()
+        time.sleep(SCAN_INTERVAL_SECONDS)
